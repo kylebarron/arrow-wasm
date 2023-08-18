fadc844094af83a5beab75488005dec8c4669986
Kyle Barron <kylebarron2@gmail.com>
2023-08-17 21:03:47 -0400

    Export memory and function table

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,19 @@
+use wasm_bindgen::prelude::*;
+
 #[cfg(feature = "arrow2")]
 pub mod arrow2;
 
 mod utils;
 
-use wasm_bindgen::prelude::*;
-
-#[wasm_bindgen]
-pub struct HelloWorld;
-
-#[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
+/// Returns a handle to this wasm instance's `WebAssembly.Memory`
+#[wasm_bindgen(js_name = _memory)]
+pub fn memory() -> JsValue {
+    wasm_bindgen::memory()
 }
 
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, test!");
+/// Returns a handle to this wasm instance's `WebAssembly.Table` which is the indirect function
+/// table used by Rust
+#[wasm_bindgen(js_name = _functionTable)]
+pub fn function_table() -> JsValue {
+    wasm_bindgen::function_table()
 }
