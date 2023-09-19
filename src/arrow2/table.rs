fe69e4d89aa83c09a037a0ed0118f19ee906608b
Kyle Barron <kylebarron2@gmail.com>
2023-09-19 17:41:05 -0600

    Improved docs with typedoc

--- src/arrow2/table.rs
+++ src/arrow2/table.rs
@@ -10,6 +10,10 @@ use arrow2::io::ipc::write::{StreamWriter as IPCStreamWriter, WriteOptions as IP
 use std::io::Cursor;
 use wasm_bindgen::prelude::*;
 
+/// A Table in WebAssembly memory conforming to the Apache Arrow spec.
+///
+/// A Table consists of one or more {@linkcode RecordBatch} objects plus a {@linkcode Schema} that
+/// each RecordBatch conforms to.
 #[wasm_bindgen]
 pub struct Table {
     schema: arrow2::datatypes::Schema,
@@ -24,6 +28,7 @@ impl Table {
         Self { schema, batches }
     }
 
+    /// Consume this table and return its components
     pub fn into_inner(
         self,
     ) -> (
@@ -36,12 +41,16 @@ impl Table {
 
 #[wasm_bindgen]
 impl Table {
-    /// Returns the schema of the record batches.
+    /// Access the Table's {@linkcode Schema}.
     #[wasm_bindgen(getter)]
     pub fn schema(&self) -> Schema {
         Schema::new(self.schema.clone())
     }
 
+    /// Access a RecordBatch from the Table by index.
+    ///
+    /// @param index The positional index of the RecordBatch to retrieve.
+    /// @returns a RecordBatch or `null` if out of range.
     #[wasm_bindgen(js_name = recordBatch)]
     pub fn record_batch(&self, index: usize) -> Option<RecordBatch> {
         let batch = self.batches.get(index)?;
@@ -49,17 +58,27 @@ impl Table {
         Some(RecordBatch::new(self.schema.clone(), batch.clone()))
     }
 
-    /// Return the number of batches in the file
+    /// The number of batches in the Table
     #[wasm_bindgen(getter, js_name = numBatches)]
     pub fn num_batches(&self) -> usize {
         self.batches.len()
     }
 
+    /// Export this Table to FFI structs according to the Arrow C Data Interface.
+    ///
+    /// This method **does not consume** the Table, so you must remember to call {@linkcode
+    /// Table.free} to release the resources. The underlying arrays are reference counted, so
+    /// this method does not copy data, it only prevents the data from being released.
     #[wasm_bindgen(js_name = toFFI)]
     pub fn to_ffi(&self) -> WasmResult<FFITable> {
         Ok(self.into())
     }
 
+    /// Export this Table to FFI structs according to the Arrow C Data Interface.
+    ///
+    /// This method **does consume** the Table, so the original Table will be
+    /// inaccessible after this call. You must still call {@linkcode FFITable.free} after
+    /// you've finished using the FFITable.
     #[wasm_bindgen(js_name = intoFFI)]
     pub fn into_ffi(self) -> WasmResult<FFITable> {
         Ok(self.into())
@@ -129,9 +148,8 @@ impl Table {
     }
 }
 
-/// Wrapper around an Arrow Table in Wasm memory (a list of FFIArrowRecordBatch objects.)
-///
-/// Refer to {@linkcode readParquetFFI} for instructions on how to use this.
+/// A representation of an Arrow Table in WebAssembly memory exposed as FFI-compatible
+/// structs through the Arrow C Data Interface.
 #[wasm_bindgen]
 pub struct FFITable(Vec<FFIRecordBatch>);
 
@@ -157,6 +175,37 @@ impl FFITable {
     }
 
     /// Get the pointer to one ArrowArray FFI struct for a given chunk index and column index
+    ///
+    /// Access the pointer to one
+    /// [`ArrowArray`](https://arrow.apache.org/docs/format/CDataInterface.html#structure-definitions)
+    /// struct representing one of the internal `RecordBatch`es. This can be viewed or copied (without serialization) to an Arrow JS `RecordBatch` by
+    /// using [`arrow-js-ffi`](https://github.com/kylebarron/arrow-js-ffi). You can access the
+    /// [`WebAssembly.Memory`](https://developer.mozilla.org/en-US/docs/WebAssembly/JavaScript_interface/Memory)
+    /// instance by using {@linkcode wasmMemory}.
+    ///
+    /// **Example**:
+    ///
+    /// ```ts
+    /// import * as arrow from "apache-arrow";
+    /// import { parseRecordBatch } from "arrow-js-ffi";
+    ///
+    /// const wasmTable: FFITable = ...
+    /// const wasmMemory: WebAssembly.Memory = wasmMemory();
+    ///
+    /// const jsBatches: arrow.RecordBatch[] = []
+    /// for (let i = 0; i < wasmTable.numBatches(); i++) {
+    ///   // Pass `true` to copy arrays across the boundary instead of creating views.
+    ///   const jsRecordBatch = parseRecordBatch(
+    ///     wasmMemory.buffer,
+    ///     wasmTable.arrayAddr(i),
+    ///     wasmTable.schemaAddr(),
+    ///     true
+    ///   );
+    ///   jsBatches.push(jsRecordBatch);
+    /// }
+    /// const jsTable = new arrow.Table(jsBatches);
+    /// ```
+    ///
     /// @param chunk number The chunk index to use
     /// @returns number pointer to an ArrowArray FFI struct in Wasm memory
     #[wasm_bindgen(js_name = arrayAddr)]
